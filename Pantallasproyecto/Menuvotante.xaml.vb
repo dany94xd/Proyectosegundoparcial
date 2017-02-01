622871769b8a089adb4f56ad0d0dd4d4502ec98f
Vinces <danimaxi60@hotmail.com>
2017-01-31 20:02:50 -0500

    correcion del menu votante

--- Pantallasproyecto/Menuvotante.xaml.vb
+++ Pantallasproyecto/Menuvotante.xaml.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Menuvotante
 
 
-    Private Sub btnpresident_Click(sender As Object, e As RoutedEventArgs) Handles btnpresident.Click
+    Private Sub btnelegir_Click(sender As Object, e As RoutedEventArgs) Handles btnelegir.Click
         Dim anterior As New gribpresidentes
         anterior.Owner = Me
         Me.Hide()
