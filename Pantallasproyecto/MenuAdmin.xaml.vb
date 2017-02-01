6cb11ca32210b46470a395e89b54812c5cac4739
Vinces <danimaxi60@hotmail.com>
2017-02-01 11:43:03 -0500

    agregacion de  pantallas menu admin menu candidato

---
+++ Pantallasproyecto/MenuAdmin.xaml.vb
@@ -0,0 +1,3 @@
+﻿Public Class MenuAdmin
+
+End Class
---
+++ Pantallasproyecto/MenuCandidato.xaml.vb
@@ -0,0 +1,8 @@
+﻿Public Class MenuCandidato
+    Private Sub btnresultados_Click(sender As Object, e As RoutedEventArgs) Handles btnresultados.Click
+        Dim anterior As New ReporteCandidato
+        anterior.Owner = Me
+        Me.Hide()
+        anterior.Show()
+    End Sub
+End Class
