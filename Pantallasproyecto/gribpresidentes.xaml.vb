7254e7f56b7731e1048c6fda97daf27635a11e82
Vinces <danimaxi60@hotmail.com>
2017-01-31 21:17:45 -0500

    Add consulta de presidentes y asambleistas usando tpanel

--- Pantallasproyecto/gribpresidentes.xaml.vb
+++ Pantallasproyecto/gribpresidentes.xaml.vb
@@ -3,11 +3,16 @@ Imports System.Data.OleDb
 
 Public Class gribpresidentes
 
-   Private dsPersonas As DataSet
+    Private dsPersonas As DataSet
 
 
     Private Sub Listapresidentes_Loaded(sender As Object, e As RoutedEventArgs) Handles Listapresidentes.Loaded
 
+
+
+    End Sub
+
+    Private Sub TabItem_Loaded(sender As Object, e As RoutedEventArgs)
         Using conexion As New OleDbConnection(ConnectionString)
 
             Dim consulta As String = "SELECT Candidato.CandidatoId, Persona.Nombres, Persona.Apellidos, PartidoPolitico.Nombre, PartidoPolitico.Lista, Cargo.Descripción
@@ -24,6 +29,29 @@ WHERE Cargo.CargoId = 2;"
 
             dataGrid.DataContext = dsPersonas
         End Using
-
     End Sub
+
+    Private Sub TabItem_Loaded_1(sender As Object, e As RoutedEventArgs)
+        Using conexion As New OleDbConnection(ConnectionString)
+
+            Dim consulta As String = "SELECT Candidato.CandidatoId, Persona.Nombres, Persona.Apellidos, PartidoPolitico.Nombre, PartidoPolitico.Lista, Cargo.Descripción
+FROM Persona INNER JOIN (PartidoPolitico INNER JOIN (Cargo INNER JOIN Candidato ON Cargo.CargoId = Candidato.CargoId) ON PartidoPolitico.PartidoId = Candidato.PartidoPoliticoId) ON Persona.PersonaId = Candidato.PersonaId
+WHERE Cargo.CargoId = 4;"
+
+            'Dim adapter As New OleDbDataAdapter(consulta, conexion)
+            Dim adapter As New OleDbDataAdapter(New OleDbCommand(consulta, conexion))
+            Dim personaCmdBuilder = New OleDbCommandBuilder(adapter)
+            dsPersonas = New DataSet("Candidato")
+            adapter.FillSchema(dsPersonas, SchemaType.Source)
+
+            adapter.Fill(dsPersonas, "Candidato")
+
+            dataGrid1.DataContext = dsPersonas
+        End Using
+    End Sub
+
+    Private Sub Listapresidentes_Closing(sender As Object, e As ComponentModel.CancelEventArgs) Handles Listapresidentes.Closing
+        End
+    End Sub
+
 End Class
