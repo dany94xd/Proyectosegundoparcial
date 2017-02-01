bfe534fa75885ccb1ab1a665f6f98c96c0971cbe
Vinces <danimaxi60@hotmail.com>
2017-02-01 13:58:37 -0500

    navegavlidad entre pantallas

--- Pantallasproyecto/AgregarCandidato.xaml.vb
+++ Pantallasproyecto/AgregarCandidato.xaml.vb
@@ -1,3 +1,8 @@
 ﻿Public Class AgregarCandidato
-
+    Private Sub Btnregresar_Click(sender As Object, e As RoutedEventArgs) Handles Btnregresar.Click
+        Dim anterior As New MenuAdmin
+        anterior.Owner = Me
+        Me.Hide()
+        anterior.Show()
+    End Sub
 End Class
--- Pantallasproyecto/MenuAdmin.xaml.vb
+++ Pantallasproyecto/MenuAdmin.xaml.vb
@@ -5,4 +5,15 @@
         Me.Hide()
         anterior.Show()
     End Sub
+
+    Private Sub btncandidato_Click(sender As Object, e As RoutedEventArgs) Handles btncandidato.Click
+        Dim anterior As New AgregarCandidato
+        anterior.Owner = Me
+        Me.Hide()
+        anterior.Show()
+    End Sub
+
+    Private Sub Window_Closing(sender As Object, e As ComponentModel.CancelEventArgs)
+        End
+    End Sub
 End Class
