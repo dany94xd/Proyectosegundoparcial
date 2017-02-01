e3b1d512d46e7b2b8970cb103bef71468b91dda7
dany <danimaxi60@hotmail.com>
2017-02-01 18:53:31 -0500

    navegabilidad candidato

--- Pantallasproyecto/AddVotante.xaml.vb
+++ Pantallasproyecto/AddVotante.xaml.vb
@@ -5,7 +5,7 @@
     End Sub
 
     Private Sub btnatras_Click(sender As Object, e As RoutedEventArgs) Handles btnatras.Click
-        Dim anterior As New Menuvotante
+        Dim anterior As New MenuAdmin
         anterior.Owner = Me
         Me.Hide()
         anterior.Show()
--- Pantallasproyecto/ReporteCandidato.xaml.vb
+++ Pantallasproyecto/ReporteCandidato.xaml.vb
@@ -1,3 +1,17 @@
 ﻿Public Class ReporteCandidato
 
+    Private Sub btnregresar_Click(sender As Object, e As RoutedEventArgs) Handles btnregresar.Click
+        Dim anterior As New MenuCandidato
+        anterior.Owner = Me
+        Me.Hide()
+        anterior.Show()
+    End Sub
+
+    Private Sub btnsalir_Click(sender As Object, e As RoutedEventArgs) Handles btnsalir.Click
+        End
+    End Sub
+
+    Private Sub winreportecand_Closing(sender As Object, e As ComponentModel.CancelEventArgs) Handles winreportecand.Closing
+        End
+    End Sub
 End Class
