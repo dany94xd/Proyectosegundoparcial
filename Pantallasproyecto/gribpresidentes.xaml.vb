78255fd5faef7f8700fe2838210cde3f86c6ecb1
Castro-C4 <castro_2003_007@hotmail.com>
2017-02-06 15:02:44 -0500

    evento load items asambleista

--- Pantallasproyecto/gribpresidentes.xaml.vb
+++ Pantallasproyecto/gribpresidentes.xaml.vb
@@ -54,4 +54,23 @@ Public Class gribpresidentes
         Return retorno
     End Function
 
+
+    Private Sub TabItem_Loaded_1(sender As Object, e As RoutedEventArgs)
+        Using conexion As New OleDbConnection(ConnectionString)
+
+            Dim consulta As String = "SELECT Candidato.CandidatoId, Persona.Nombres, Persona.Apellidos, PartidoPolitico.Nombre, PartidoPolitico.Lista, Cargo.Descripción, Cargo.CargoId FROM Persona INNER JOIN (PartidoPolitico INNER JOIN (Cargo INNER JOIN Candidato ON Cargo.CargoId = Candidato.CargoId) ON PartidoPolitico.PartidoId = Candidato.PartidoPoliticoId) ON Persona.PersonaId = Candidato.PersonaId WHERE Cargo.CargoId = 4;"
+            'Dim adapter As New OleDbDataAdapter(consulta, conexion)
+            Dim adapter As New OleDbDataAdapter(New OleDbCommand(consulta, conexion))
+            Dim personaCmdBuilder = New OleDbCommandBuilder(adapter)
+            dsPersonas = New DataSet("Candidato")
+            'adapter.FillSchema(dsPersonas, SchemaType.Source)
+
+            adapter.Fill(dsPersonas, "Candidato")
+
+            dataGrid1.DataContext = dsPersonas
+        End Using
+    End Sub
+
+
+
 End Class
