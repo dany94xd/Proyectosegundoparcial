63044f588d4bb8f6b51da93f297886851b5e62af
Dany <danimaxi60@hotmail.com>
2017-02-06 16:05:52 -0500

    metodo loaded para cargar el reporte de presidentes

--- Pantallasproyecto/ReporteAdmin.xaml.vb
+++ Pantallasproyecto/ReporteAdmin.xaml.vb
@@ -1,5 +1,8 @@
-﻿Public Class ReporteAdmin
+﻿Imports System.Data
+Imports System.Data.OleDb
 
+Public Class ReporteAdmin
+    Private dsCandidatos As DataSet
     Private Sub btnregresar_Click(sender As Object, e As RoutedEventArgs) Handles btnregresar.Click
         Dim anterior As New MenuAdmin
         anterior.Owner = Me
@@ -15,5 +18,24 @@
 
     Private Sub Window_Closing(sender As Object, e As ComponentModel.CancelEventArgs)
         End
+    End Sub
+
+
+    Private Sub Gridpresi_Loaded(sender As Object, e As RoutedEventArgs) Handles Gridpresi.Loaded
+        Using conexion As New OleDbConnection(ConnectionString)
+
+            Dim consulta As String = "SELECT Cargo.Descripción, PartidoPolitico.Nombre, Persona.Nombres, Persona.Apellidos, Count(1) AS Conteo FROM Persona INNER JOIN ((PartidoPolitico INNER JOIN (Cargo INNER JOIN Candidato ON Cargo.CargoId = Candidato.CargoId) ON PartidoPolitico.PartidoId = Candidato.PartidoPoliticoId) INNER JOIN Voto ON (Cargo.CargoId = Voto.CargoId) AND (Candidato.CandidatoId = Voto.CandidatoId)) ON Persona.PersonaId = Candidato.PersonaId WHERE(((Voto.CargoId) = 2)) GROUP BY Cargo.Descripción, PartidoPolitico.Nombre, Persona.Nombres, Persona.Apellidos ORDER BY Count(1) DESC;"
+            'Dim adapter As New OleDbDataAdapter(consulta, conexion)
+            Dim adapter As New OleDbDataAdapter(New OleDbCommand(consulta, conexion))
+            Dim personaCmdBuilder = New OleDbCommandBuilder(adapter)
+            dsCandidatos = New DataSet("Candidato")
+            'adapter.FillSchema(dsPersonas, SchemaType.Source)
+
+            adapter.Fill(dsCandidatos, "Candidato")
+
+
+            Gridpresi.DataContext = dsCandidatos
+        End Using
+
     End Sub
 End Class
