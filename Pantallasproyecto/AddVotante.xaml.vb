f2f40de1bf854a933332924057d292c668640242
Dany <danimaxi60@hotmail.com>
2017-02-06 16:12:50 -0500

    modificaciones de la pantalla y agregacion del metodo initialized para cargar la lista de votantes

--- Pantallasproyecto/AddVotante.xaml.vb
+++ Pantallasproyecto/AddVotante.xaml.vb
@@ -1,4 +1,7 @@
-﻿Public Class AddVotante
+﻿Imports System.Data
+Imports System.Data.OleDb
+
+Public Class AddVotante
 
     Private Sub winagregarvotante_Closing(sender As Object, e As ComponentModel.CancelEventArgs) Handles winagregarvotante.Closing
         End
@@ -13,6 +16,24 @@
 
     Private Sub btningresar_Click(sender As Object, e As RoutedEventArgs) Handles btningresar.Click
 
+
+    End Sub
+
+    Private Sub Grid_Initialized(sender As Object, e As EventArgs)
+        Dim consulta As String = "SELECT Persona.[PersonaId], Persona.[Cedula], Persona.[Apellidos], Persona.[Nombres] FROM Persona;"
+
+        Using conexion As New OleDbConnection(ConnectionString)
+
+            Dim adapter As New OleDbDataAdapter(New OleDbCommand(consulta, conexion))
+            Dim personaCmdBuilder = New OleDbCommandBuilder(adapter)
+            Dim dsCandidatos = New DataSet("Candidato")
+            'adapter.FillSchema(dsPersonas, SchemaType.Source)
+
+            adapter.Fill(dsCandidatos, "Candidato")
+
+
+            gridPersona.DataContext = dsCandidatos
+        End Using
 
     End Sub
 End Class
