98f2a796a95047cd2a56f13c436248be6701cb79
Dany <danimaxi60@hotmail.com>
2017-02-06 16:06:40 -0500

    datagrib consulta admin para asambleistas

--- Pantallasproyecto/ReporteAdmin.xaml.vb
+++ Pantallasproyecto/ReporteAdmin.xaml.vb
@@ -38,4 +38,22 @@ Public Class ReporteAdmin
         End Using
 
     End Sub
+
+    Private Sub gribasam_Loaded(sender As Object, e As RoutedEventArgs) Handles gribasam.Loaded
+        Using conexion As New OleDbConnection(ConnectionString)
+
+            Dim consulta As String = "SELECT Cargo.Descripción, PartidoPolitico.Nombre, Persona.Nombres, Persona.Apellidos, Count(1) AS Conteo FROM Persona INNER JOIN ((PartidoPolitico INNER JOIN (Cargo INNER JOIN Candidato ON Cargo.CargoId = Candidato.CargoId) ON PartidoPolitico.PartidoId = Candidato.PartidoPoliticoId) INNER JOIN Voto ON (Cargo.CargoId = Voto.CargoId) AND (Candidato.CandidatoId = Voto.CandidatoId)) ON Persona.PersonaId = Candidato.PersonaId WHERE(((Voto.CargoId) = 4)) GROUP BY Cargo.Descripción, PartidoPolitico.Nombre, Persona.Nombres, Persona.Apellidos ORDER BY Count(1) DESC;"
+
+            'Dim adapter As New OleDbDataAdapter(consulta, conexion)
+            Dim adapter As New OleDbDataAdapter(New OleDbCommand(consulta, conexion))
+            Dim personaCmdBuilder = New OleDbCommandBuilder(adapter)
+            dsCandidatos = New DataSet("Candidato")
+            'adapter.FillSchema(dsPersonas, SchemaType.Source)
+
+            adapter.Fill(dsCandidatos, "Candidato")
+
+
+            gribasam.DataContext = dsCandidatos
+        End Using
+    End Sub
 End Class
