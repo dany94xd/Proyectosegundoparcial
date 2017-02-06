3fa0499cb320364e478564fad10d1906b0d868d4
Castro-C4 <castro_2003_007@hotmail.com>
2017-02-06 15:41:15 -0500

    datagrid colsulta candidato

--- Pantallasproyecto/ReporteCandidato.xaml.vb
+++ Pantallasproyecto/ReporteCandidato.xaml.vb
@@ -1,17 +1,43 @@
-﻿Public Class ReporteCandidato
+﻿Imports System.Data.OleDb
+Imports System.Data
 
-    Private Sub btnregresar_Click(sender As Object, e As RoutedEventArgs) Handles btnregresar.Click
-        Dim anterior As New MenuCandidato
-        anterior.Owner = Me
-        Me.Hide()
-        anterior.Show()
-    End Sub
+Public Class ReporteCandidato
+    Private dsCandidatos As DataSet
 
-    Private Sub btnsalir_Click(sender As Object, e As RoutedEventArgs) Handles btnsalir.Click
-        End
-    End Sub
 
     Private Sub winreportecand_Closing(sender As Object, e As ComponentModel.CancelEventArgs) Handles winreportecand.Closing
         End
     End Sub
+
+
+
+    Private Sub datagribconsulta_Loaded(sender As Object, e As RoutedEventArgs) Handles datagribconsulta.Loaded
+        Using conexion As New OleDbConnection(ConnectionString)
+
+
+
+
+
+            Dim consulta As String = "SELECT Cargo.Descripción, PartidoPolitico.Nombre, Persona.Nombres, Persona.Apellidos, Count(1) AS Conteo FROM Persona INNER JOIN ((PartidoPolitico INNER JOIN (Cargo INNER JOIN Candidato ON Cargo.CargoId = Candidato.CargoId) ON PartidoPolitico.PartidoId = Candidato.PartidoPoliticoId) INNER JOIN Voto ON (Candidato.CandidatoId = Voto.CandidatoId) AND (Cargo.CargoId = Voto.CargoId)) ON Persona.PersonaId = Candidato.PersonaId WHERE(((Voto.CargoId) = " + Cargo.ToString + ")) GROUP BY Cargo.Descripción, PartidoPolitico.Nombre, Persona.Nombres, Persona.Apellidos ORDER BY Count(1) DESC;"
+            'Dim adapter As New OleDbDataAdapter(consulta, conexion)
+            Dim adapter As New OleDbDataAdapter(New OleDbCommand(consulta, conexion))
+            Dim personaCmdBuilder = New OleDbCommandBuilder(adapter)
+            dsCandidatos = New DataSet("Candidato")
+            'adapter.FillSchema(dsPersonas, SchemaType.Source)
+
+            adapter.Fill(dsCandidatos, "Candidato")
+
+
+            datagribconsulta.DataContext = dsCandidatos
+        End Using
+
+    End Sub
+
+    Private Sub btnmenuprincipal_Click(sender As Object, e As RoutedEventArgs) Handles btnmenuprincipal.Click
+        Dim anterior As New MenuUser
+        anterior.Owner = Me
+        Me.Hide()
+        anterior.Show()
+    End Sub
 End Class
+
