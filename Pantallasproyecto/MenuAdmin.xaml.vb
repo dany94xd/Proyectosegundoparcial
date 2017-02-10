b6b00f83fc97565f633b75853f5381b0fbdff603
cccastro09 <castro_2003_007@hotmail.com>
2017-02-10 10:35:37 -0500

    Actulizacion de ventanas

--- Pantallasproyecto/MenuAdmin.xaml.vb
+++ Pantallasproyecto/MenuAdmin.xaml.vb
@@ -6,12 +6,7 @@
         anterior.Show()
     End Sub
 
-    Private Sub btncandidato_Click(sender As Object, e As RoutedEventArgs) Handles btncandidato.Click
-        Dim anterior As New AgregarCandidato
-        anterior.Owner = Me
-        Me.Hide()
-        anterior.Show()
-    End Sub
+
 
     Private Sub Window_Closing(sender As Object, e As ComponentModel.CancelEventArgs)
         End
